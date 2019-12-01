d0da594b0e98fc6a3134d19f05fbc3de7cda38cb
yoko <yokodayo02@gmail.com>
2019-12-01 15:57:45 +0900

    test

---
+++ nybgaming/Desktop/nybgame/Assets/キャラチップ/character1/MainCharacter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MainCharacter : MonoBehaviour
+{
+    void Update()
+    {
+        Transform myTransform = this.transform;
+        Vector3 pos = myTransform.position;
+        if (Input.GetKeyDown(KeyCode.D)){
+            pos.x += 1f;
+        }else if (Input.GetKeyDown(KeyCode.A))
+        {
+            pos.x -= 1f;
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            pos.y += 1f;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            pos.y -= 1f;
+        }
+        myTransform.position = pos;
+    }
+    private void OnCollisionEnter2D(Collision collision)
+    {
+        
+    }
+}
